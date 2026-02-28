a6cb14b82df43163c73e23cc68aad90f7bd4cb46
Sawyer Hood <sawyerjhood@gmail.com>
2026-02-27 16:17:59 -0800

    chore: remove unused dead code files

--- apps/backend/src/integrations/slack/index.ts
+++
@@ -1,8 +0,0 @@
-export { SlackIntegrationService } from "./slack-integration.js";
-export { markdownToSlackMrkdwn } from "./slack-mrkdwn.js";
-export type { SlackStatusEvent } from "./slack-status.js";
-export type {
-  SlackChannelDescriptor,
-  SlackConnectionTestResult,
-  SlackIntegrationConfigPublic
-} from "./slack-types.js";
